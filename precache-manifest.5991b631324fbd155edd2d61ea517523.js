894cd283d24f47fc145f2a62ac257360bcc9788f
ArnyMac <max@MacBook-Air-ArnyMac.local>
2019-10-20 22:57:48 +0300

    Updates

---
+++ precache-manifest.5991b631324fbd155edd2d61ea517523.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eef6ece4046b96ee86f0fbaa1af8c67",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "2f8442ec40502e0b5bf5",
+    "url": "/react_uber-eats/static/css/main.7dcee84e.chunk.css"
+  },
+  {
+    "revision": "20121d9766eff97c8109",
+    "url": "/react_uber-eats/static/js/2.27653720.chunk.js"
+  },
+  {
+    "revision": "2f8442ec40502e0b5bf5",
+    "url": "/react_uber-eats/static/js/main.341827bb.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
